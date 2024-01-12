eb2877eb5e8a0862371ddc4d2e8fab423c53efd3
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-12 23:03:45 +0100

    [Update] Adding a testClass for memoize method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,7 +10,7 @@ from typing import (
     Callable,
 )
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import Mock, patch
 
 
@@ -63,3 +63,35 @@ class TestGetJson(unittest.TestCase):
         with patch('requests.get', return_value=mock):
             response = get_json(test_url)
             self.assertEqual(response, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Test case for memoize function
+    """
+    def test_memoize(self):
+        """ Mehtod to test the memoize test cases
+        """
+
+        class TestClass:
+            """ A class"""
+
+            def a_method(self):
+                """ A method"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ A proporty"""
+                return self.a_method()
+
+        testClass = TestClass()
+
+        with patch.object(testClass, 'a_method') as mock_method:
+            mock_method.return_value = 42
+
+            res1 = testClass.a_property
+            res2 = testClass.a_property
+
+            self.assertEqual(res1, 42)
+            self.assertEqual(res2, 42)
+            mock_method.assert_called_once()
